2982027d77a2ca3b461f7a74e0d986d45a1e2d96
ajay kumar <ajaykumar.study38@gmail.com>
2023-11-22 21:44:37 +0530

    hi i am shubm

---
+++ App.js
@@ -0,0 +1,13 @@
+    const parent = React.createElement("div", {id: "parent"}, [
+        React.createElement("div", {id: "child"},[
+            React.createElement("h1", {id: "abc"}, "Hi this is h1"),
+            React.createElement("h2", {id: "bcd"}, "this is h2"),
+        ]),
+        React.createElement("div", {id: "child"},[
+            React.createElement("h1", {id: "abc"}, "Hi this is h1"),
+            React.createElement("h2", {id: "bcd"}, "this is h2"),
+        ]),
+    ]);
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
